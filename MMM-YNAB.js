c51804a34816a6d8b48af92653a39f04c6e5f112
Dana Hanna <dana.hanna@gmail.com>
2021-04-22 19:49:25 -0400

    Update MMM-YNAB.js
    add some logging

--- MMM-YNAB.js
+++ MMM-YNAB.js
@@ -11,8 +11,9 @@ Module.register("MMM-YNAB", {
     getDom: function () {
         var wrapper = document.createElement("div");
         wrapper.classList = ["xsmall"];
+	console.log('Data length: ' + this.data.items.length);
         if (this.data.items && this.data.items.length > 0) {
-			for (let i of this.data.items) {
+	    for (let i of this.data.items) {
                 wrapper.innerHTML = this.data.items.map(a => "<span class='ynab-name'>" + a.name + "</span><span class='ynab-balance'>$" + (a.balance/1000).toFixed(2) + "</span>").join('');
             }
         }
@@ -21,6 +22,7 @@ Module.register("MMM-YNAB", {
 
     socketNotificationReceived: function (notification, payload) {
         console.log("notification: " + notification);
+        console.log("payload: " + payload);
         if (notification == "UPDATE") {
             this.data = payload;
             this.updateDom(0);
